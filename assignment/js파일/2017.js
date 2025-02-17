aca72a7f7fc67a0a8a0042a31fe16faa049d5923
Leeoc1 <lch5752@gmail.com>
2025-02-17 14:26:20 +0900

    aa

---
+++ assignment/js파일/2017.js
@@ -0,0 +1,63 @@
+var hi = "환영합니다.";
+
+var a = true;
+var b = false;
+var c = 10 > 5;
+var d = Boolean(null);
+
+var num = 100;
+var str = "자바스크립트";
+
+var num1 = 15;
+var num2 = 2;
+result1 = num1 + num2;
+result2 = num1 - num2;
+result3 = num1 * num2;
+result4 = num1 / num2;
+result5 = num1 % num2;
+
+var t1 = "학교종이 ";
+var t2 = "땡땡땡 ";
+var t3 = 8282;
+var t4 = " 어서모이자~";
+result6 = t1 + t2 + t3 + t4;
+
+const year = 2002;
+const country = "대한민국";
+const eventName = "월드컵";
+const str_1 = `${year}년 ${country} ${eventName} 개최`;
+
+const age = 30;
+const after = 10;
+const str_2 = `현재 나이는 ${age}세 이지만<br>
+${after}년 후에는 ${age + after}세가 됩니다.`;
+
+var nunum1 = 10;
+var nunum2 = 3;
+
+var str = "<table border='1'>";
+str += "<tr>";
+str += "<td>1</td><td>2</td><td>3</td>";
+str += "</tr>";
+str += "</table>";
+
+var num1 = 10;
+var num2 = 20;
+var result;
+
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+var result;
+
+var a = 10;
+var b = 3;
+
+var height = 180;
+var weight = 74;
+
+var name = prompt("성함을 적어주세요", "");
+var height = prompt("키를 적어주세요", "");
+var weight = prompt("몸무게를 적어주세요", "");
+var normal_w = (height - 100) * 0.9;
---
+++ assignment/js파일/2017_1.js
@@ -0,0 +1,67 @@
+x = 5;
+y = 6;
+z = x + y;
+document.getElementById("demo").innerHTML = "The value of z is: " + z;
+
+var x1 = 5;
+var y1 = 6;
+var z1 = x1 + y1;
+document.getElementById("demo1").innerHTML = "The value of z is: " + z;
+
+let x2 = 5;
+let y2 = 6;
+let z2 = x2 + y2;
+document.getElementById("demo2").innerHTML = "The value of z is: " + z;
+
+const x3 = 5;
+const y3 = 6;
+const z3 = x3 + y3;
+document.getElementById("demo3").innerHTML = "The value of z is: " + z;
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+document.getElementById("demo4").innerHTML = "The total is: " + total;
+
+const pi = 3.14;
+let person = "John Doe";
+let answer = "Yes I am!";
+
+document.getElementById("demo5").innerHTML =
+  pi + "<br>" + person + "<br>" + answer;
+
+let carName = "Volvo";
+document.getElementById("demo6").innerHTML = carName;
+
+let person1 = "John Doe",
+  carName1 = "Volvo",
+  price = 200;
+document.getElementById("demo7").innerHTML = carName;
+
+let carName2;
+document.getElementById("demo8").innerHTML = carName2;
+
+var carName3 = "Volvo";
+var carName3;
+document.getElementById("demo9").innerHTML = carName3;
+
+let x4 = 5 + 2 + 3;
+document.getElementById("demo10").innerHTML = x4;
+
+let x5 = "John" + " " + "Doe";
+document.getElementById("demo11").innerHTML = x5;
+
+let x6 = "5" + 2 + 3;
+document.getElementById("demo12").innerHTML = x6;
+
+let x7 = 2 + 3 + "5";
+document.getElementById("demo13").innerHTML = x7;
+
+let $ = 2;
+let $$ = 5;
+document.getElementById("demo14").innerHTML = $ + $$;
+
+let _x = 2;
+let _100 = 5;
+
+document.getElementById("demo15").innerHTML = _x + _100;
