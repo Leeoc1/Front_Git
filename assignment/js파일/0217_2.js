5b0696a2c9782449a8229e3f0edf4cabaff52728
Leeoc1 <lch5752@gmail.com>
2025-02-17 15:44:48 +0900

    adwa

---
+++ assignment/js파일/0217_2.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  document.getElementById("panel").style.display = "block";
+}
+
+function removeElement() {
+  document.getElementById("imgbox1").style.display = "none";
+}
+
+function changeVisibility() {
+  document.getElementById("imgbox2").style.visibility = "hidden";
+}
+
+function resetElement() {
+  document.getElementById("imgbox1").style.display = "block";
+  document.getElementById("imgbox2").style.visibility = "visible";
+}
