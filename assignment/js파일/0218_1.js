255afbd27301bb189c18046653a04b3c2d5ab169
Leeoc1 <lch5752@gmail.com>
2025-02-18 16:52:06 +0900

    a

---
+++ assignment/js파일/0218_1.js
@@ -0,0 +1,37 @@
+const element = document.getElementById("intro");
+document.getElementById("demo").innerHTML =
+  "The text from the intro paragraph is: " + element.innerHTML;
+
+const d1 = document.getElementById("q2");
+const element1 = d1.getElementsByTagName("p");
+document.getElementById("demo1").innerHTML =
+  "The text in first paragraph (index 0) is: " + element1[0].innerHTML;
+
+const d2 = document.getElementById("q3");
+const element2 = d2.getElementsByTagName("p");
+document.getElementById("demo3").innerHTML =
+  'The first paragraph (index 0) inside "main" is: ' + element2[0].innerHTML;
+
+const d3 = document.getElementsByClassName("intro");
+document.getElementById("demo4").innerHTML =
+  'The first paragraph (index 0) with class="intro" is: ' + d3[0].innerHTML;
+
+const d4 = document.querySelectorAll("p.intro2");
+document.getElementById("demo5").innerHTML =
+  'The first paragraph (index 0) with class="intro" is: ' + d4[0].innerHTML;
+
+const d5 = document.forms["frm1"];
+let text = "";
+for (let i = 0; i < d5.length; i++) {
+  text += d5.elements[i].value + "<br>";
+}
+document.getElementById("demo6").innerHTML = text;
+
+document.getElementById("p1").innerHTML = "New text!";
+
+document.getElementById("id01").innerHTML = "New Heading";
+
+document.getElementById("image").src =
+  "https://www.w3schools.com/js/landscape.jpg";
+
+document.getElementById("demo7").innerHTML = "Date: " + Date();
