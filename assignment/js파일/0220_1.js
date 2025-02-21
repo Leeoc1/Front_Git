6d0ebfefcf57986cda927d799414e862dcc98486
Leeoc1 <lch5752@gmail.com>
2025-02-21 12:50:45 +0900

    aaf

--- assignment/js파일/0220_1.js
+++ assignment/js파일/0220_1.js
@@ -44,4 +44,79 @@ ${cupYear}년 ${cupMonth}월 ${cupDate}일 ${cupDayInKor}요일`;
 
 // --------------------s3------------------------
 
-const lastDate = new Date(nowYear, 12, 31);
+const lastDate = new Date(nowYear, 11, 31);
+const diffDate = lastDate.getTime() - today.getTime();
+
+const remaining_days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
+document.getElementById(
+  "s3"
+).innerHTML += `<h1>연말까지 남은 일수</h1>연말까지 ${remaining_days}일 남았습니다.`;
+
+// --------------------s4------------------------
+
+const ex_num = 2.1234;
+const max = Math.max(10, 5, 8, 30),
+  min = Math.min(10, 5, 8, 30),
+  round = Math.round(ex_num),
+  floor = Math.floor(ex_num),
+  ceil = Math.ceil(ex_num),
+  rand = Math.random(),
+  pi = Math.PI;
+
+document.getElementById(
+  "s4"
+).innerHTML += `<h1>수학 객체</h1> max값 = ${max}<br>
+min값 = ${min}<br>
+round값 = ${round}<br>
+floor값 = ${floor}<br>
+ceil값 = ${ceil}<br>
+random값 = ${rand}<br>
+PI값 = ${pi}<br>`;
+
+// --------------------s5------------------------
+
+function playGame() {
+  const game = document.getElementById("userChoice").value;
+  let gameNum;
+
+  // 사용자의 선택을 숫자로 변환
+  switch (game) {
+    case "가위":
+      gameNum = 1;
+      break;
+    case "바위":
+      gameNum = 2;
+      break;
+    case "보":
+      gameNum = 3;
+      break;
+  }
+
+  // 컴퓨터의 선택을 숫자로 설정
+  const com = Math.ceil(Math.random() * 3);
+  const resultDiv = document.getElementById("result");
+
+  // 결과 초기화
+  resultDiv.innerHTML = "";
+
+  // 사용자와 컴퓨터의 선택을 이미지로 표시
+  resultDiv.innerHTML += `<p>나:</p><img src="./images/math_img_${gameNum}.jpg" alt="사용자 선택">`;
+  resultDiv.innerHTML += `<p>컴퓨터:</p><img src="./images/math_img_${com}.jpg" alt="컴퓨터 선택">`;
+
+  // 결과 판단 및 메세지 출력
+  if (gameNum == com) {
+    resultDiv.innerHTML += "<p>무승부</p>";
+  } else {
+    let result;
+    if (
+      (gameNum === 1 && com === 3) ||
+      (gameNum === 2 && com === 1) ||
+      (gameNum === 3 && com === 2)
+    ) {
+      result = "승리!";
+    } else {
+      result = "패배...";
+    }
+    resultDiv.innerHTML += `<p>${result}</p>`;
+  }
+}
